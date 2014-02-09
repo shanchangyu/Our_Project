4cdcac1bd716d1a92e259d9fe2f5d83b5acb4759
chenzhuogit <1359763362@qq.com>
2014-02-09 16:47:53 +0800

    增加问价

---
+++ those_people/thosepeople/com/thosepeople/controller/HomePageInitializer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.thosepeople.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author chenzhuo
+ *
+ */
+@Controller
+@RequestMapping(value="/index")
+public class HomePageInitializer {
+	
+	@RequestMapping(value="/initialize")
+   public ModelAndView initialize(){
+	   return new ModelAndView("index");
+   }
+}
