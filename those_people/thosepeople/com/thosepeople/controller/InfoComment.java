e178f6ff865ae6df4f3479ea83d63e004335ab94
1359763362@qq.com <zhuo198919>
2014-04-12 16:34:03 +0800

    修改评论模块 1.最新评论放在最上边
    2.评论显示调整

--- those_people/thosepeople/com/thosepeople/controller/InfoComment.java
+++ those_people/thosepeople/com/thosepeople/controller/InfoComment.java
@@ -1,5 +1,6 @@
 package com.thosepeople.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.thosepeople.exception.BusinessException;
 import com.thosepeople.service.PostCommentService;
+import com.thosepeople.util.DateUtils;
+import com.thosepeople.vo.UserInfo;
 
 @Controller
 @RequestMapping("/post_comment")
@@ -37,7 +40,6 @@ public class InfoComment {
 			@RequestParam("infoType") int infoType,
 			@RequestParam("commentType") int commentType, HttpSession session)
 			throws BusinessException {
-		System.out.println("fffffff");
 		if(beRepliedUserId==null)
 		{
 			beRepliedUserId=-1;
@@ -56,6 +58,13 @@ public class InfoComment {
 				response.put("beRepliedUserName", beRepliedUserName);
 				response.put("beRepliedUserId", String.valueOf(beRepliedUserId));
 			}
+			UserInfo userInfo=(UserInfo) session.getAttribute("userInfo");
+			String replyUserHeadPic=userInfo.getHeadPicPath();
+			String replyUserNickName=userInfo.getNickName();
+			String replyTime=DateUtils.getNowDateString(new Date());
+			response.put("replyUserHeadPic", replyUserHeadPic);
+			response.put("replyUserNickName", replyUserNickName);
+			response.put("replyTime",replyTime);
 			response.put("commentContent", commentContent);
 			return response;
 		} else {
